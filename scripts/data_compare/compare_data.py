b58febcfbe4bdc67191cd385e1bb95edba5dfd5f
nachoaz <iarangur2@gmail.com>
2017-11-25 14:09:15 -0500

    adding data cross-checking script

---
+++ scripts/data_compare/compare_data.py
@@ -0,0 +1,47 @@
+# compare_data.py
+
+import sys
+sys.path.append('../')
+
+import numpy as np
+import pandas as pd
+
+from utils.general_utils import get_qrtr_from_date
+
+pd.set_option('expand_frame_repr', False)
+
+
+def read_srow_df(df_path):
+    srow_df = pd.read_excel(df_path)
+    srow_df = srow_df.loc[:, srow_df.columns[::-1]]
+    srow_df.columns = list(map(get_qrtr_from_date, 
+        [date.split(' ')[0] for date in map(str, srow_df.columns)]))
+    return srow_df
+
+
+def check_map(feats_map, row_cstat_ibm, srow_df, sqrtr, eqrtr):
+    for cstat_feat, srow_feat in feats_map.items():
+        cstat_val = row_cstat_ibm[cstat_feat].values[0]*1000000
+        srow_val = sum(srow_df.loc[srow_feat][sqrtr:eqrtr])
+        if np.isclose(cstat_val, srow_val):
+            print("YES! {} is close to {}.".format(cstat_feat, srow_feat))
+        else:
+            print("{} is not close to {}.".format(cstat_feat, srow_feat))
+
+# define dataframes
+ibm_income = read_srow_df('../../data/company_data/nyse/IBM/srow_data/IBM_income.xlsx')
+cstat_ibm = pd.read_csv('sample-data-2.dat', sep=' ', index_col='date')
+row_cstat_ibm = cstat_ibm.loc['201204':'201204','saleq_ttm':]
+
+# check trailing twelve months features
+ttm_feats_map = {
+        'saleq_ttm': 'Revenues',
+        'cogsq_ttm': 'Cost of Revenue',
+        'xsgaq_ttm': 'Selling, General and Administrative Expense',
+        'niq_ttm': 'Net Income'
+        }
+check_map(ttm_feats_map, row_cstat_ibm, ibm_income, '2011-Q1', '2011-Q4')
+
+# check most recent quarter features
+mrq_feats_map = {
+        }
