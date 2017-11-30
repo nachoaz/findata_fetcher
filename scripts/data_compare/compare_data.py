ecb10e8a378847f673996d53a0818c34608bc148
nachoaz <iarangur2@gmail.com>
2017-11-29 19:35:00 -0500

    cleaned up comparison

--- scripts/data_compare/compare_data.py
+++ scripts/data_compare/compare_data.py
@@ -3,7 +3,6 @@
 import sys
 sys.path.append('../')
 
-import numpy as np
 import pandas as pd
 
 from utils.general_utils import get_qrtr_from_date
@@ -19,33 +18,89 @@ def read_srow_df(df_path):
     return srow_df
 
 
-def check_map(feats_map, row_cstat_ibm, srow_df, sqrtr, eqrtr):
+def print_comparison_report(cstat_feat, srow_feat, diff):
+    if abs(diff) < 5:
+        print("'{}' is close to {} (diff: {:0.4f})".format(
+            cstat_feat, srow_feat, diff))
+    else:
+        print("\t- '{}' is not close to {} (diff: {:0.4f})".format(
+            cstat_feat, srow_feat, diff))
+
+
+def check_mrq_map(feats_map, cstat_series, balance_path, eqrtr):
+    srow_df = read_srow_df(balance_path)
+
     for cstat_feat, srow_feat in feats_map.items():
-        cstat_val = row_cstat_ibm[cstat_feat].values[0]*1000000
+        cstat_val = cstat_series[cstat_feat+'_mrq']*1000000
+        srow_val = srow_df.loc[srow_feat][eqrtr]
+        diff = ((srow_val - cstat_val) / (srow_val + 0.00001)) * 100
+        print_comparison_report(cstat_feat, srow_feat, diff)
+
+
+def check_ttm_map(feats_map, cstat_series, income_path, sqrtr, eqrtr):
+    srow_df = read_srow_df(income_path)
+
+    for cstat_feat, srow_feat in feats_map.items():
+        cstat_val = cstat_series[cstat_feat+'_ttm']*1000000
         srow_val = sum(srow_df.loc[srow_feat][sqrtr:eqrtr])
         diff = (srow_val - cstat_val) / srow_val * 100
-        if np.isclose(cstat_val, srow_val):
-            print("YES! {} is close to {}.".format(cstat_feat, srow_feat))
+        print_comparison_report(cstat_feat, srow_feat, diff)
+
+
+def check_maps(map_kind, curr_month, sqrtr, eqrtr):
+    cstat_df = pd.read_csv('sample-data-2.dat', sep=' ', index_col='date')
+    cstat_series = cstat_df.loc[curr_month, 'saleq_ttm':]
+
+    income_path = 'IBM_income.xlsx'
+    balance_path = 'IBM_balance.xlsx'
+
+    for feats_map, kind in map_kind:
+        if kind == 'ttm':
+            check_ttm_map(feats_map, cstat_series, income_path, sqrtr, eqrtr)
         else:
-            print("{} is not close to {}.".format(cstat_feat, srow_feat))
+            check_mrq_map(feats_map, cstat_series, balance_path, eqrtr)
 
-        print("diff: {:0.4f".format(diff))
 
-# define dataframes
-ibm_income = read_srow_df('IBM_income.xlsx')
-cstat_ibm = pd.read_csv('sample-data-2.dat', sep=' ', index_col='date')
-row_cstat_ibm = cstat_ibm.loc['201204':'201204','saleq_ttm':]
 
-# check trailing twelve months features
+# trailing twelve months features
 ttm_feats_map = {
-        'saleq_ttm': 'Revenues',
-        'cogsq_ttm': 'Cost of Revenue',
-        'xsgaq_ttm': 'Operating Expenses',
-        'oiadpq_ttm': 'Operating Income',
-        'niq_ttm': 'Net Income'
+        'saleq': 'Revenues',
+        'cogsq': 'Cost of Revenue',
+        'xsgaq': 'Operating Expenses',
+        'oiadpq': 'Operating Income',
+        'niq': 'Net Income'
         }
-check_map(ttm_feats_map, row_cstat_ibm, ibm_income, '2011-Q1', '2011-Q4')
 
-# check most recent quarter features
+# most recent quarter features
 mrq_feats_map = {
+        'cheq': 'Cash and Equivalents',
+        'rectq': 'Trade and Non-Trade Receivables',
+        'invtq': 'Inventory',
+        'acoq': 'Investments Current',
+        'ppentq': 'Property, Plant & Equipment Net',
+        'aoq': 'Assets Non-Current',
+        'dlcq': 'Debt Current',
+        'apq': 'Trade and Non-Trade Payables',
+        'txpq': 'Tax Liabilities',
+        'lcoq': 'Deferred Revenue',
+        'ltq': 'Total Liabilities'
         }
+
+# cross check data available at start of april 2012
+curr_month = 201201
+sqrtr = '2010-Q4'
+eqrtr = '2011-Q3'
+maps_to_check = [(ttm_feats_map, 'ttm'), (mrq_feats_map, 'mrq')]
+
+check_maps(maps_to_check, curr_month, sqrtr, eqrtr)
+
+# see which ones most closely match
+balance_df = read_srow_df('IBM_balance.xlsx')
+cstat_ibm = pd.read_csv('sample_data-2.dat', sep=' ', index_col='date')
+cstat_series = cstat_ibm.loc[201204,'saleq_ttm':]
+
+for feat, val in cstat_series['cheq_mrq':].items():
+     srow_closest = balance_df.loc[:, eqrtr].index[np.argmin(abs((balance_df.loc[:, eqrtr].values / 1000000) - val))]
+     srow_val = balance_df.loc[srow_closest, eqrtr]/1000000
+     diff = ((srow_val - val) / (srow_val + 0.00000001)) * 100
+     print("'{}' closest to: '{}' (diff: {:0.4f} %)".format(feat, srow_closest, diff))
