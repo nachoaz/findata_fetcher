ef1e5c0f1f80cc68dd7ddb817c81a757b669bdee
nachoaz <iarangur2@gmail.com>
2018-01-15 15:46:24 -0500

    changes onto writing close price and price change percents datafiles.

--- scripts/utils/p_ch_pcts_utils.py
+++ scripts/utils/p_ch_pcts_utils.py
@@ -4,33 +4,35 @@ import os
 
 import pandas as pd
 
+from utils.general_utils import mkdir_if_not_exists
 
-def write_and_get_adj_cp_csv(adj_cppath, quandl_path, logpath):
+
+def write_adj_cp_csv_get_df(adj_cppath, quandlpath, logpath):
     stat_pre = "\t- Writing {}".format(adj_cppath)
     try:
-        df = pd.read_csv(quandl_path)
+        df = pd.read_csv(quandlpath)
         df_to_write = df.loc[:, ['Date', 'Adj. Close']]
         df_to_write.columns = ['date', 'price']
         df_to_write.to_csv(adj_cppath, index=False)
         print(stat_pre + ': SUCCESSFUL')
 
-    except:
+    except FileNotFoundError:
         df_to_write = None
-        print(stat_pre + ': FAILED')
+        status = stat_pre + ": FAILED (file not found)."
+        print(status)
         with open(logpath, 'a') as f:
-            f.write(stat_pre + ": FAILED\n")
+            f.write(status + "\n")
             
     return df_to_write
 
 
-def get_prices_df(adj_cp_df, adj_cppath, periodicity='monthly'):
-    "returns prices_df (for all available business month dates)"
-    if adj_cp_df is None:
-        prices_df = pd.read_csv(adj_cppath, header=0, names=['date', 'price'])
-    else:
-        prices_df = adj_cp_df
-
-    prices_df = prices_df.set_index('date')
+def get_prices_df(adj_cp_df, periodicity='monthly'):
+    """
+    Returns df with price at end of business period (for all available business
+    dates). For example: to get df with price at the business-end-date of every
+    month, specify `periodicity='monthly'`.
+    """
+    prices_df = adj_cp_df.set_index('date')
     prices_df.index = pd.to_datetime(prices_df.index)
     
     if periodicity == 'monthly':
@@ -39,9 +41,9 @@ def get_prices_df(adj_cp_df, adj_cppath, periodicity='monthly'):
     return prices_df
 
 
-def get_p_ch_pcts_df(adj_cp_df, adj_cppath):
-    "returns p_ch_pcts_df for tkr"
-    prices_df = get_prices_df(adj_cp_df, adj_cppath)
+def get_p_ch_pcts_df(adj_cp_df):
+    "Returns p_ch_pcts_df for given adj_cp_df."
+    prices_df = get_prices_df(adj_cp_df)
 
     for col, periods in [
             ('pct_ch_one', 1),
@@ -55,37 +57,49 @@ def get_p_ch_pcts_df(adj_cp_df, adj_cppath):
     return prices_df
 
 
-def write_p_ch_pcts_csv(logpath, p_ch_pctspath, adj_cp_df, adj_cppath):
-    "writes tkr_p_ch_pcts.csv for tkr, if it doesn't already exist"
+def write_p_ch_pcts_csv(adj_cp_df, p_ch_pctspath, logpath):
+    "Writes tkr_p_ch_pcts.csv for tkr."
     stat_pre = '\t- Writing {}'.format(p_ch_pctspath)
 
     try:
-        p_ch_pcts_df = get_p_ch_pcts_df(adj_cp_df, adj_cppath)
+        p_ch_pcts_df = get_p_ch_pcts_df(adj_cp_df)
         p_ch_pcts_df.to_csv(p_ch_pctspath, index=False)
         print(stat_pre + ': SUCCESSFUL')
 
-    except:
-        print(stat_pre + ': FAILED')
+    except AttributeError:
+        status = stat_pre + ": FAILED (adj_cp file not found)."
+        print(status)
         with open(logpath, 'a') as g:
-            g.write(stat_pre + ': FAILED' + '\n')
+            g.write(status + '\n')
 
 
 def write_adj_cps_and_p_ch_pcts_csvs(tkr, tkrdir, logpath, overwrite):
-    "writes adj_cp.csv and p_ch_pcts.csv if the don't already exist"
-    quandl_dir = os.path.join(tkrdir, "quandl_data")
-    quandl_path = os.path.join(quandl_dir, "{}_quandl.csv".format(tkr))
-    adj_cppath = os.path.join(tkrdir, "cp_data/{}_adj_cp.csv".format(tkr))
-    p_ch_pctspath = os.path.join(tkrdir, "cp_data/{}_p_ch_pcts.csv".format(tkr))
+    "Writes adj_cp.csv and p_ch_pcts.csv if they don't already exist."
+    # create cp_data directory if not already existent
+    cp_datadir = os.path.join(tkrdir, "cp_data")
+    mkdir_if_not_exists(cp_datadir)
 
-    # write adj_cp.csv
-    if not os.path.exists(adj_cppath) or overwrite:
-        adj_cp_df = write_and_get_adj_cp_csv(adj_cppath, quandl_path, logpath)
+    # define necessary paths
+    quandlpath = os.path.join(tkrdir, "quandl_data", tkr + "_quandl.csv")
+    adj_cppath = os.path.join(cp_datadir, tkr + "_adj_cp.csv")
+    p_ch_pctspath = os.path.join(cp_datadir, tkr + "_p_ch_pcts.csv")
+
+    # write adj_cp.csv (if necessary) and get adj_cp_df
+    if os.path.isfile(adj_cppath):
+        if overwrite:
+            adj_cp_df = write_adj_cp_csv_get_df(adj_cppath, quandlpath, logpath)
+        else:
+            print("\t- {} already exists.".format(adj_cppath))
+            adj_cp_df = pd.read_csv(adj_cppath, header=0, 
+                                    names=['date', 'price'])
     else:
-        print("\t- {} already exists.".format(adj_cppath))
-        adj_cp_df = None
+        adj_cp_df = write_adj_cp_csv_get_df(adj_cppath, quandlpath, logpath)
 
     # write p_ch_pcts_csv
-    if not os.path.exists(p_ch_pctspath) or overwrite:
-        write_p_ch_pcts_csv(logpath, p_ch_pctspath, adj_cp_df, adj_cppath)
+    if os.path.isfile(p_ch_pctspath):
+        if overwrite:
+            write_p_ch_pcts_csv(adj_cp_df, p_ch_pctspath, logpath)
+        else:
+            print("\t- {} already exists.".format(p_ch_pctspath))
     else:
-        print("\t- {} already exists.".format(p_ch_pctspath))
+        write_p_ch_pcts_csv(adj_cp_df, p_ch_pctspath, logpath)
--- scripts/write_cp_datafiles.py
+++ scripts/write_cp_datafiles.py
@@ -5,6 +5,7 @@ import os
 import argparse
 
 from utils.general_utils import get_tkrs_from_clist, \
+                                rm_file_if_exists, \
                                 mkdir_if_not_exists, \
                                 LOGS_DIR, CDATA_DIR
 from utils.p_ch_pcts_utils import write_adj_cps_and_p_ch_pcts_csvs
@@ -13,25 +14,25 @@ from utils.p_ch_pcts_utils import write_adj_cps_and_p_ch_pcts_csvs
 def main(clist_pofix, overwrite):
 
     for mkt in ["nasdaq", "nyse"]:
+        # get list of tkrs to write cp_datafiles for
         clist = "{}_{}".format(mkt, clist_pofix)
         tkrs = get_tkrs_from_clist(clist)
 
-        logpath = os.path.join(LOGS_DIR, "write_cp_datafiles_log_"+clist)
-        if os.path.exists(logpath):
-            os.remove(logpath)
+        # remove logfile
+        logpath = os.path.join(LOGS_DIR, "write_cp_datafiles_log_" + clist)
+        rm_file_if_exists(logpath)
 
+        # fill cp_data directories with cp_datafiles
         if tkrs:
             print("\nWriting datafiles for {}...".format(mkt))
         else:
             print("\nNo tkrs found for {}.".format(mkt))
 
         for tkr in tkrs:
+            tkrdir = os.path.join(CDATA_DIR, mkt, tkr)
+
+            # write cp_datafiles
             print("Writing datafile for {} ({})...".format(tkr, mkt))
-            tkrdir = os.path.join(CDATA_DIR, "{}/{}".format(mkt, tkr))
-
-            cp_datadir = os.path.join(tkrdir, "cp_data")
-            mkdir_if_not_exists(cp_datadir)
-
             write_adj_cps_and_p_ch_pcts_csvs(tkr, tkrdir, logpath, overwrite)
 
 
