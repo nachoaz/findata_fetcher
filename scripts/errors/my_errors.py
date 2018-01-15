c77a96eec7595b81f6f5f8b5433e5acc20356f55
nachoaz <iarangur2@gmail.com>
2018-01-15 14:57:29 -0500

    incorporated exceptions to manage and track errors in download process.

---
+++ scripts/errors/my_errors.py
@@ -0,0 +1,6 @@
+# my_errors.py
+"""Contains class definitions for errors that I use throughout."""
+
+
+class HTTPResponseNotOkException(Exception):
+    pass
--- scripts/utils/download_data_utils.py
+++ scripts/utils/download_data_utils.py
@@ -7,14 +7,21 @@ import time
 import requests
 
 from utils.general_utils import report_and_register_error
+from errors.my_errors import HTTPResponseNotOkException
 
 
 def download_file_from_url(url, filepath):
     """Downloads file at url, saves at filepath."""
     time.sleep(3)
     response = requests.get(url)
-    with open(filepath, 'wb') as f:
-        f.write(response.content)
+    if response.status_code == 200:
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+    elif response.status_code == 404:
+        raise HTTPResponseNotOkException("HTTP Status 404: Not Found.")
+    else:
+        raise HTTPResponseNotOkException(
+                "HTTP Status: {}".format(response.status_code))
 
 
 def download_and_report_outcome(url, filepath, logpath, stat_pre):
@@ -25,5 +32,5 @@ def download_and_report_outcome(url, filepath, logpath, stat_pre):
     try:
         download_file_from_url(url, filepath)
         print(stat_pre + ": SUCCESSFUL")
-    except requests.HTTPError as e:
+    except HTTPRepsonseNotOkException as e:
         report_and_register_error(stat_pre, e, logpath)
